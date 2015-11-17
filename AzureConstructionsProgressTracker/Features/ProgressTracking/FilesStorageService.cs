0c5de9fff3fb033f43f5747ddcf27f3daa7fd06b
test <github@noreply.github.com>
2015-11-17 11:12:17 +0100

    storage

--- AzureConstructionsProgressTracker/Features/ProgressTracking/FilesStorageService.cs
+++ AzureConstructionsProgressTracker/Features/ProgressTracking/FilesStorageService.cs
@@ -2,6 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Web;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System.Configuration;
 
 namespace AzureConstructionsProgressTracker.Features.ProgressTracking
 {
@@ -18,7 +23,26 @@ namespace AzureConstructionsProgressTracker.Features.ProgressTracking
             // - create container
             // - upload blob
             // - return URL
-            throw new NotImplementedException();
+            //  throw new NotImplementedException();
+
+            // Retrieve storage account from connection string.
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+             ConfigurationManager.AppSettings["StorageConnectionString"]);
+
+            // Retrieve storage account from connection string.
+           
+
+            // Create the blob client.
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            // Retrieve reference to a previously created container.
+            CloudBlobContainer container = blobClient.GetContainerReference("acp");
+
+            // Retrieve reference to a blob named "myblob".
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference(fileName);
+            await blockBlob.UploadFromStreamAsync(file.InputStream);
+
+            return blockBlob.Uri.ToString();
         }
     }
 }
